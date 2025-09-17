477a26248ac2226e8b107a344b35ab22b58f9cf0
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-09-17 13:40:44 +0900

    fix : [250917] 계층적 예외(Exception)처리 로직 구축

--- service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,7 +3,6 @@ package org.delivery.api.account;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.api.common.api.Api;
-import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ service/api/src/main/java/org/delivery/api/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.delivery.api.exceptionhandler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.common.error.ErrorCode;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice  // 예외를 해당 클래스로 다 모음
+@Order (value = Integer.MAX_VALUE) // 가장 마지막에 실행 적용(기본값)  // 여러가지 예외에 대한 핸들러의 순서를 지정 (값이 낮을수록 최우선)
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Api<Object>> exception(
+            Exception exception // spring 주입
+    ){
+
+        log.error("",exception);
+        return ResponseEntity
+                .status(500)
+                .body(
+                        Api.ERROR(ErrorCode.SERVER_ERROR)
+                );
+    }
+}
