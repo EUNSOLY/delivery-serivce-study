85d0f83240de7828ce7027060fa2522c3ffc9be1
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-09-17 10:40:00 +0900

    fix : [250917] API 에러코드 로직화 하기

--- service/api/src/main/java/org/delivery/api/common/api/Api.java
+++ service/api/src/main/java/org/delivery/api/common/api/Api.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCodeInterface;
 
 // 최상위
 @Data
@@ -19,6 +20,29 @@ public class Api<T> {
         var api = new Api<T>();
         api.result = Result.OK();
         api.body = data;
+        return api;
+    }
+
+    public static Api<Object> ERROR(Result result){
+        var api = new Api<Object>();
+        api.result = result;
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeInterface errorCodeInterface){
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeInterface);
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeInterface errorCodeInterface, Throwable tx){
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeInterface,tx);
+        return api;
+    }
+    public static Api<Object> ERROR(ErrorCodeInterface errorCodeInterface,String des){
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeInterface,des);
         return api;
     }
 }
--- service/api/src/main/java/org/delivery/api/common/api/Result.java
+++ service/api/src/main/java/org/delivery/api/common/api/Result.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.ErrorCodeInterface;
 
 @Data
 @AllArgsConstructor
@@ -18,9 +20,37 @@ public class Result {
 
     public static Result OK(){
         return Result.builder()
-                .resultCode(200)
-                .resultMessage("OK")
+                .resultCode(ErrorCode.OK.getErrorCode())
+                .resultMessage(ErrorCode.OK.getDescription())
                 .resultDescription("성공")
                 .build();
     }
+    // 기본 에러
+    public static Result ERROR(ErrorCodeInterface errorCodeInterface){
+        return Result.builder()
+                .resultCode(errorCodeInterface.getErrorCode())
+                .resultMessage(errorCodeInterface.getDescription())
+                .resultDescription("성공")
+                .build();
+    }
+
+    // 예외가 발생했을 때
+    public static Result ERROR(ErrorCodeInterface errorCodeInterface, Throwable tx){
+        return Result.builder()
+                .resultCode(errorCodeInterface.getErrorCode())
+                .resultMessage(errorCodeInterface.getDescription())
+                .resultDescription(tx.getLocalizedMessage()) //추천하지는 않는 메세지 사용
+                .build();
+    }
+
+    // 에러문구 삽입
+    public static Result ERROR(ErrorCodeInterface errorCodeInterface, String des){
+        return Result.builder()
+                .resultCode(errorCodeInterface.getErrorCode())
+                .resultMessage(errorCodeInterface.getDescription())
+                .resultDescription(des)
+                .build();
+    }
+
+
 }
