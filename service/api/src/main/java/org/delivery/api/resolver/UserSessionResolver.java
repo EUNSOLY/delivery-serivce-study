920cf11cd60669d4ae2a8c0dd80fcc7712242d63
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-09-18 11:35:33 +0900

    fix: [250918] 사용자 인증 로직 header 검증 로직 resolver로 분리하여 공용화

---
+++ service/api/src/main/java/org/delivery/api/resolver/UserSessionResolver.java
@@ -0,0 +1,55 @@
+package org.delivery.api.resolver;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.annotation.UserSession;
+import org.delivery.api.domain.user.model.User;
+import org.delivery.api.domain.user.service.UserService;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+//Request 요청이 들어오면 실행(AOP방식으로 동작)
+public class UserSessionResolver implements HandlerMethodArgumentResolver {
+    private final UserService userService;
+    @Override
+
+    public boolean supportsParameter(MethodParameter parameter) {
+        // 지원하는 파라미터 체크, 어노테이션 체크
+
+        // 1. 어노테이션이 있는지 체크 (UserSession이라는 어노테이션이 달려있는지 체크)
+        var annotation = parameter.hasParameterAnnotation(UserSession.class);
+        // 2. 파마미터의 타입 체크 (매개변수로 들어오는 파라미터가 User.class가 맞는지 확인)
+        var parameterType = parameter.getParameterType().equals(User.class);
+        return (annotation && parameterType);
+    }
+
+    @Override
+    //supportsParameter 에서 true 반환시 실행
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        // request context holder에서 찾아오기
+        var requestContext = RequestContextHolder.getRequestAttributes();
+        var userId = requestContext.getAttribute("userId", RequestAttributes.SCOPE_REQUEST);
+
+        var userEntity = userService.getUserWithThrow(Long.parseLong(userId.toString()));
+
+        // 사용자 정보 셋팅
+        return User.builder()
+                .id(userEntity.getId())
+                .name(userEntity.getName())
+                .email(userEntity.getEmail())
+                .password(userEntity.getPassword())
+                .address(userEntity.getAddress())
+                .status(userEntity.getStatus())
+                .registeredAt(userEntity.getRegisteredAt())
+                .unregisteredAt(userEntity.getUnregisteredAt())
+                .lastLoginAt(userEntity.getLastLoginAt())
+                .build();
+    }
+}
