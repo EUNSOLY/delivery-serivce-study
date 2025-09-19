d932e36f12da9b1c497fea605c325e6f4f6ad538
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-09-19 13:41:23 +0900

    feat: [250918] security config 구성 - 스웨거, 오픈에이피아이등은 인증없이 접속 허가 구성 - JPA 사용경로 config 추가

---
+++ service/store-admin/src/main/java/org/delivery/storeadmin/config/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.delivery.storeadmin.config.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity // security 활성화
+public class SecurityConfig {
+    private final List<String> SWAGGER = List.of(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)// 기본값은 활성화
+                .authorizeHttpRequests((it) ->
+                        it.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                                .permitAll()// resource 에 대해서는 모든 요청 허용
+                        .requestMatchers(SWAGGER.toArray(new String[0]))
+                                .permitAll() // 스웨거는 인증 없이 통과
+                                .requestMatchers("/open-api/**").permitAll() // open-api 하위 모든 주소는 인증 없이 통과
+                                .anyRequest().authenticated()// 그외 모든 요청은 인증 사용
+                )
+                .formLogin(Customizer.withDefaults())
+                ;
+
+        return httpSecurity.build();
+    }
+}
