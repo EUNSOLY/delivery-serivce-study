ebb2a7214720c13ecc5d617788e8cee5f02dc850
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-10-01 10:50:19 +0900

    feat: 공통모듈에 코틀린을 통한 에러코드 인터페이스, 이넘, Api 반환 구조 생성

---
+++ service/common/src/main/kotlin/org/delivery/common/annotaion/Business.kt
@@ -0,0 +1,5 @@
+package org.delivery.common.annotaion
+
+
+// @Service가 spring 의존성을 갖고이 있기 때문에 common 에서는 순수 java + kotlin만 사용하기 위해서 어노테이션 생성 중단
+annotation class Business()
---
+++ service/common/src/main/kotlin/org/delivery/common/api/Api.kt
@@ -0,0 +1,6 @@
+package org.delivery.common.api
+
+data class Api<T> (
+    var result: Result?=null,
+    var body : T? = null
+)
