9dfcf276af0d4be022994a0c5e55a902b4c839d5
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-10-01 23:16:44 +0900

    feat: API GW 모듈 생성

---
+++ service/apigw/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+group = "com.delivery"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc") [Tomcat]
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux") // NettyWebServer
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
